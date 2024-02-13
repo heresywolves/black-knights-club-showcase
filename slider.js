3fb3596ec130367f54e0b163c3f6a7fe1b0c00aa
Howl Kimura <howlkimuradev@gmail.com>
2024-02-13 17:58:45 +0300

    implement working infinite scrolling cycle in slider

--- slider.js
+++ slider.js
@@ -3,21 +3,25 @@ const slider = () => {
   const prevBtn = document.querySelector('.slider button.left');
   const nextBtn = document.querySelector('.slider button.right');
 
-  let currentIndex = 1;
+  let currentIndex = 6;
   const offsetDistance = 414;
 
   // Set initial position
-  slider.style.transform = `translateX(${6 * -offsetDistance}px)`;
+  slider.style.transform = `translateX(${currentIndex * -offsetDistance}px)`;
 
 
   // Event listeners for next and previous buttons
   prevBtn.addEventListener('click', () => {
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
       currentIndex--;
       console.log('current index: ' + currentIndex)
       updateSlider();
   });
 
   nextBtn.addEventListener('click', () => {
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
       currentIndex++;
       console.log('current index: ' + currentIndex)
       updateSlider();
@@ -31,15 +35,17 @@ const slider = () => {
 
   // Reset position on transition end for smooth looping
   slider.addEventListener('transitionend', () => {
-      if (currentIndex === 0) {
-          currentIndex = 6;
-          slider.style.transition = 'none';
-          slider.style.transform = `translateX(${-currentIndex * offsetDistance}px)`;
-      } else if (currentIndex === slider.children.length - 1) {
-          currentIndex = 1;
-          slider.style.transition = 'none';
-          slider.style.transform = `translateX(${-currentIndex * offsetDistance}px)`;
-      }
+    nextBtn.disabled = false;
+    prevBtn.disabled = false;
+    if (currentIndex === 0) {
+        currentIndex = 6;
+        slider.style.transition = 'none';
+        slider.style.transform = `translateX(${-currentIndex * offsetDistance}px)`;
+    } else if (currentIndex === 12) {
+        currentIndex = 6;
+        slider.style.transition = 'none';
+        slider.style.transform = `translateX(${-currentIndex * offsetDistance}px)`;
+    }
   });
 };
 slider();
