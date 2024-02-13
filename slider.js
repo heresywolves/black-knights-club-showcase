88f0a963f34b2b6698bc716a8b2284fcf110dbd1
Howl Kimura <howlkimuradev@gmail.com>
2024-02-13 18:06:33 +0300

    make updates happen on page display in the slider

--- slider.js
+++ slider.js
@@ -2,38 +2,49 @@ const slider = () => {
   const slider = document.querySelector('.slider .moving-wrapper');
   const prevBtn = document.querySelector('.slider button.left');
   const nextBtn = document.querySelector('.slider button.right');
+  const pageDisplay = document.querySelector('.slider span.current')
 
   let currentIndex = 6;
   const offsetDistance = 414;
+  let page = 1;
 
-  // Set initial position
   slider.style.transform = `translateX(${currentIndex * -offsetDistance}px)`;
 
+  function updatePageDisplay() {
+    if (page < 1) {
+      page = 6;
+    } else if (page > 6) {
+      page = 1;
+    }
+    pageDisplay.textContent = page;
+  }
 
-  // Event listeners for next and previous buttons
+  updatePageDisplay();
+
+
   prevBtn.addEventListener('click', () => {
       nextBtn.disabled = true;
       prevBtn.disabled = true;
+      page--;
+      updatePageDisplay();
       currentIndex--;
-      console.log('current index: ' + currentIndex)
       updateSlider();
   });
 
   nextBtn.addEventListener('click', () => {
       nextBtn.disabled = true;
       prevBtn.disabled = true;
+      page++;
+      updatePageDisplay();
       currentIndex++;
-      console.log('current index: ' + currentIndex)
       updateSlider();
   });
 
-  // Function to update slider position
   function updateSlider() {
       slider.style.transition = 'transform 0.5s ease-in-out';
       slider.style.transform = `translateX(${-currentIndex * offsetDistance}px)`;
   }
 
-  // Reset position on transition end for smooth looping
   slider.addEventListener('transitionend', () => {
     nextBtn.disabled = false;
     prevBtn.disabled = false;
